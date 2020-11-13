135be8274e8180f5dd0f61dd20d9a6e58dc88b79
a-tichat <gamecup84@gmail.com>
2020-11-13 18:41:39 +0700

    fix bug in some image

--- sudokudetector/Sudoku.py
+++ sudokudetector/Sudoku.py
@@ -1,5 +1,6 @@
 from imutils.perspective import four_point_transform
 from skimage.segmentation import clear_border
+import matplotlib.pyplot as plt
 import numpy as np
 import imutils
 import cv2
@@ -22,21 +23,26 @@ class Sudoku:
         gray = cv2.cvtColor(self.full_image, cv2.COLOR_BGR2GRAY)
 
         # Thrashold
-        blurred = cv2.GaussianBlur(gray, (7, 7), 3)
-        thresh = cv2.adaptiveThreshold(
-            blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 57, 5)
-        thresh = cv2.bitwise_not(thresh)
+
+        thresh = self.adap_threshold(gray)
+        # self.debug(thresh)
 
         cnts = self.find_contour(thresh)
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
+        output = self.full_image.copy()
+
         puzzleCnt = None
         for c in cnts:
             peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            approx = cv2.approxPolyDP(c, 0.06 * peri, True)
+
             if len(approx) == 4:
                 puzzleCnt = approx
                 break
+
+        if puzzleCnt is None:
+            raise Exception("Sudoku puzzle not found."+self.image_path)
 
         # self.image_bgr = four_point_transform(
         #     self.full_image, puzzleCnt.reshape(4, 2))
@@ -52,6 +58,7 @@ class Sudoku:
     def filter_number(self, cell):
         _, thresh = cv2.threshold(cell, 0, 255,
                                   cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
         thresh = clear_border(thresh)
 
         cnts = self.find_contour(thresh)
@@ -63,7 +70,7 @@ class Sudoku:
 
         c = max(cnts, key=cv2.contourArea)
 
-        if cv2.contourArea(c) < 20:
+        if cv2.contourArea(c) < 40:
             return self.remove_image(thresh)
 
         cv2.drawContours(mask, [c], -1, 255, -1)
@@ -77,15 +84,24 @@ class Sudoku:
     def remove_image(self, thresh):
         return cv2.bitwise_and(thresh, np.zeros(thresh.shape, np.uint8))
 
+    def adap_threshold(self, gray):
+        blurred = cv2.GaussianBlur(gray, (7, 7), 3)
+        thresh = cv2.adaptiveThreshold(
+            blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 57, 5)
+        return cv2.bitwise_not(thresh)
+
+    def debug(self, image):
+        plt.imshow(image, cmap='gray')
+
     def split_cells(self):
-        x_size = self.image_binary.shape[0] // 9
-        y_size = self.image_binary.shape[1] // 9
+        x_size = self.image_gray.shape[0] // 9
+        y_size = self.image_gray.shape[1] // 9
 
         array_cell = np.zeros(shape=(9, 9), dtype=object)
 
         for x in range(0, 9):
             for y in range(0, 9):
-                cell = self.image_binary[x_size *
-                                         x:x_size * (x+1), y_size * y:y_size * (y+1)]
-                array_cell[x][y] = self.filter_number(cell)
+                cell = self.image_gray[x_size *
+                                       x:x_size * (x+1), y_size * y:y_size * (y+1)]
+                array_cell[x][y] = cell
         self.cells = array_cell
